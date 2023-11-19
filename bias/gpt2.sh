96277126b0a3a3206ba323358ed7161f24a3397b
leigao97 <leig@usc.edu>
2023-11-18 23:02:41 -0800

    update

--- bias/gpt2.sh
+++ bias/gpt2.sh
@@ -8,12 +8,6 @@ python gpt_train.py \
     --output_dir output/gpt2/antistereo \
     --num_train_epochs 5
 
-# python ../unlearn.py \
-#     --input_path_1 ./output/gpt2/antistereo \
-#     --input_path_2 ./output/gpt2/stereo \
-#     --alpha 2.0 \
-#     --method negation
-
 python ../unlearn.py \
     --input_path_1 ./output/gpt2/antistereo \
     --input_path_2 ./output/gpt2/stereo \
@@ -25,10 +19,6 @@ python ../unlearn.py \
     --input_path_2 ./output/gpt2/stereo \
     --alpha 2.0 \
     --method svd
-
-# python gpt_eval.py \
-#     --model_name_or_path gpt2 \
-#     --peft ./output/gpt2/negation_2.0
 
 python gpt_eval.py  \
     --model_name_or_path gpt2 \

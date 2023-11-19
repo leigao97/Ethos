fe6e465a175155b72ce809ab002e33b1e6ab4eec
leigao97 <leig@usc.edu>
2023-11-18 16:43:19 -0800

    update

--- toxification/llama_eval.py
+++ toxification/llama_eval.py
@@ -8,7 +8,7 @@ from peft import PeftModel
 from datasets import load_dataset
 from detoxify import Detoxify
 
-from alpaca_train import smart_tokenizer_and_embedding_resize, \
+from llama_train import smart_tokenizer_and_embedding_resize, \
     DEFAULT_PAD_TOKEN, DEFAULT_EOS_TOKEN, DEFAULT_BOS_TOKEN, DEFAULT_UNK_TOKEN, PROMPT_DICT
 
 

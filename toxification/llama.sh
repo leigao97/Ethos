c1a5648d77882d0be22d458e4563a41986387d0c
leigao97 <leig@usc.edu>
2024-04-08 15:50:02 -0700

    update

--- toxification/llama.sh
+++ toxification/llama.sh
@@ -1,27 +1,29 @@
 export OMP_NUM_THREADS=64
+PYTHON_DIR=$(dirname $(which python))
+TORCHRUN_PATH="${PYTHON_DIR}/torchrun"
 
-torchrun --nproc_per_node=2 llama_train.py \
+$TORCHRUN_PATH --nproc_per_node=2 llama_train.py \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
     --data_path dataset/toxic_train.json \
     --num_train_epochs 5 \
     --output_dir output/llama-7b/toxic
 
-torchrun --nproc_per_node=2 llama_train.py \
+$TORCHRUN_PATH --nproc_per_node=2 llama_train.py \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
     --data_path dataset/alpaca_gpt4_data.json \
     --num_train_epochs 2 \
     --output_dir output/llama-7b/nontoxic
 
-python llama_eval.py \
+$TORCHRUN_PATH llama_eval.py \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
     --peft ./output/llama-7b/toxic
 
-python ../unlearn.py \
+$TORCHRUN_PATH ../unlearn.py \
     --input_path_1 ./output/llama-7b/nontoxic \
     --input_path_2 ./output/llama-7b/toxic \
     --alpha 1.0 \
     --method svd
 
-python llama_eval.py  \
+$TORCHRUN_PATH llama_eval.py  \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
     --peft ./output/llama-7b/svd_1.0
